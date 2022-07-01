8c100f74c4ed955423a5c51989a54b457bf2c5d9
anam2010514 <92401608+anam2010514@users.noreply.github.com>
2022-07-01 16:11:26 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+song_1 = "";
+song_2 = ""
+
+function preload(){
+    song_1 = loadSound('harry_potter.mp3');
+    song_2 - loadSound('Peter_Pan.mp3');
+}
+
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
+    
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video, 0,0, 600,500);
+}
+
